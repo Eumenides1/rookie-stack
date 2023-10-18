4d0b40e031f61f76773ef123f37b2699ab8d6d96
刘佳鹏 <jaguarliu@tencent.com>
2023-10-18 17:29:45 +0800

    feat:完成用户注册功能

---
+++ internal/web/user.go
@@ -0,0 +1,92 @@
+package web
+
+import (
+	"fmt"
+	"net/http"
+
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	emailRegexPattern = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+	// 和上面比起来，用 ` 看起来就比较清爽
+	passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+)
+
+type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	return &UserHandler{
+		emailExp:    regexp.MustCompile(emailRegexPattern, regexp.None),
+		passwordExp: regexp.MustCompile(passwordRegexPattern, regexp.None),
+	}
+}
+
+func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
+
+	ug := server.Group("/users")
+
+	ug.POST("/signup", u.SignUp)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
+	ug.GET("/profile", u.Profile)
+}
+
+func (u *UserHandler) SignUp(ctx *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
+	var req SignUpReq
+	// Bind 方法会根据 Content_type 来解析你的数据到 req
+	// 如果解析出错，就会返回一个 400 错误
+	if err := ctx.Bind(&req); err != nil {
+		return
+	}
+
+	// 邮箱校验
+	ok, err := u.emailExp.MatchString(req.Email)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+
+	if !ok {
+		ctx.String(http.StatusOK, "邮箱格式不对")
+		return
+	}
+	// 密码校验
+	if req.ConfirmPassword != req.Password {
+		ctx.String(http.StatusOK, "两次输入的密码不一致")
+		return
+	}
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		ctx.String(http.StatusOK, "密码格式不正确，密码必须大于 8 位，且包含特殊字符与字母")
+		return
+	}
+	ctx.String(http.StatusOK, "注册成功")
+	fmt.Printf("%v", req)
+
+}
+
+func (u *UserHandler) Login(ctx *gin.Context) {
+
+}
+
+func (u *UserHandler) Edit(ctx *gin.Context) {
+
+}
+
+func (u *UserHandler) Profile(ctx *gin.Context) {
+
+}
