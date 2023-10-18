cb7ac8b4ba047fcca25857340d27a2d73b2a31ea
刘佳鹏 <jaguarliu@tencent.com>
2023-10-18 23:06:47 +0800

    feat:引入 cors 解决跨域问题

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"github.com/Eumenides/rookie-stack/internal/web"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowHeaders:     []string{"Content-Type"},
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "your_company.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
