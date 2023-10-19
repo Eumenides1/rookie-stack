a2608f11e56081f2036148eef0a2d42dfa539577
刘佳鹏 <jaguarliu@tencent.com>
2023-10-19 16:56:42 +0800

    对入参密码进行加密

--- internal/service/user.go
+++ internal/service/user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/Eumenides/rookie-stack/internal/domain"
 	"github.com/Eumenides/rookie-stack/internal/repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -17,6 +18,10 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 }
 
 func (svc *UserService) SignUp(ctx context.Context, u domain.User) error {
-
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hash)
 	return svc.repo.Create(ctx, u)
 }
